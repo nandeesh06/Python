1157d3677faf62cedd05a33fa5e805433c7043b6
nandeesh06 <nandeeshkv06@gmail.com>
2025-07-03 12:15:52 +0530

    modified file

--- iterators_and_generators.py
+++ iterators_and_generators.py
@@ -1,47 +1,32 @@
 
 
-
+# Iterators
+# ------------
+"""An iterator is an object that can be looped through one item at a time using next().
+ Any object with __iter__() and __next__() methods is an iterator"""
+#Ex:
+nums = [10,20,30]
+it_nums = iter(nums) # this makes an iterator
+print(next(it_nums))
+# 10
+print(next(it_nums))
+# 20
+print(next(it_nums))
+# 30
 
 # Generators
+# -------------
+"""Generators in Python are a powerful and memory-efficient way to create iterators. Instead of returning all values at 
+once, generators yield items one at a time, only when requested.
+What is a Generator?
+A generator is a function that uses the yield keyword instead of return."""
 
-# Generators in Python are a powerful and memory-efficient way to create iterators.
-# Instead of returning all values at once, generators yield items one at a time, only when requested.
+def even_numbers(n):
+    for i in range(n):
+        if i%2 == 0:
+            yield i
 
-# What is a Generator?
-# A generator is a function funtion that uses the yield keyword instead of return.
-
-
-# Ex:
-
-nums = [7, 8, 9, 10]
-it= iter(nums)
-print(it.__next__())
-print(it.__next__())
-print(it.__next__())
-print(next(it))
-
-class TopTen:
-
-    def __init__(self):
-        self.num =1
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.num <= 10:
-            val = self.num
-            self.num +=1
-
-            return val
-        else:
-            raise StopIteration
-
-values = TopTen()
-
-print(next(values))
-
-for i in values:
+for i in even_numbers(10):
     print(i)
 
 
